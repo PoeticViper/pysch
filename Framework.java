0e1fe0a938a624d1685d32a5a7400c824df05f67
PoeticViper <37876024+PoeticViper@users.noreply.github.com>
2018-04-24 12:11:53 -0500

    Update Framework.java

--- Framework.java
+++ Framework.java
@@ -1,6 +1,9 @@
+import javafx.*;
 
+class Framework {
 
-public static void main(String[] args)
-{
-
+  public static void main(String[] args)
+    {
+  System.out.println("Just a fat test, wont hold the main function at the end");
+    }
 }
