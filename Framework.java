0fe97ea84a51a1f88118d05b3586277e9bf714cc
PoeticViper <37876024+PoeticViper@users.noreply.github.com>
2018-04-24 12:14:29 -0500

    Update Framework.java

--- Framework.java
+++ Framework.java
@@ -4,6 +4,6 @@ class Framework {
 
   public static void main(String[] args)
     {
-  System.out.println("Just a fat test, wont hold the main function at the end");
+      System.out.println("Just a fat test, wont hold the main function at the end");
     }
 }
