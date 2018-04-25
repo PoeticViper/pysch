4db4013ab99f804c7f5fb3c2d5cb1d30e6d4ff75
PoeticViper <joshrmcdaniel@gmail.com>
2018-04-25 11:27:14 -0500

    Started working on the stage framework

--- Framework.java
+++ Framework.java
@@ -1,9 +1,19 @@
-import javafx.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-class Framework {
+class Framework extends Application{
+	Button back = new Button("Back");
+ 	Button next = new Button("Next");
+	GridPane pane = new GridPane();
 
-  public static void main(String[] args)
+  public void start(Stage primaryStage)
     {
       System.out.println("Just a fat test, wont hold the main function at the end");
     }
+ 
 }
